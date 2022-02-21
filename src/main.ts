81a7c4a215e1001da85f61fff6a7d6e4844d2742
Adam Cox <adam@adamcox.net>
2022-02-21 15:13:39 -0500

    impl and doc't openapi tooling

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  
+  const docOptions: SwaggerDocumentOptions = {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
   const options = new DocumentBuilder()
     .setTitle('Toybox')
     .setDescription('A demo backend via Nestjs')
     .setVersion('1.0')
     //.addTag('contact')
     .build();
-  const document = SwaggerModule.createDocument(app, options);
+  const document = SwaggerModule.createDocument(app, options, docOptions);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
